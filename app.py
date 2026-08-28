07255937e9cf0626d270d698fd6c4788da3c752a
watashifitra45-dot <watashifitra45@gmail.com>
2026-08-28 23:49:37 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    nama = "AZZAHRA RAMADHANI"
+    pesan = [ "🎉🎉.Happy Birthday Zah.🎉🎉",
+     "Terimakasih telah bertahan sampai sejauh ini.",
+     "Kamu kuat, kamu hebat.",
+     "Semoga di usia yang baru ini.",
+     "Kamu selalu di kelilingi hal-hal baik.",
+     "Tetaplah tersenyum dan bahagia, walau terkadang dunia tak berpihak padamu.",
+     "Semoga semua impian dan cita-citamu segera terwujud.",
+     "AAMMIIN."
+    ]
+    return render_template('index.html', nama=nama, pesan=pesan)
+
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
