f82f078dd90e5bf2f836515c30355f9ce657871b
watashifitra45-dot <watashifitra45@gmail.com>
2026-08-29 00:42:16 +0700

    Refactor app.py for Flask application setup

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def home():
      "AAMMIIN."
     ]
     return render_template('index.html', nama=nama, pesan=pesan)
-
+    
 app = app
-
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
